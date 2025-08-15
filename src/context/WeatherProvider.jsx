03e62d01acf8d8088f86cfd29d5444912089904f
nieckita <niecki.tehrany.bildung@gmail.com>
2025-08-15 10:37:58 +0200

    commentiren

--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -1,5 +1,12 @@
 import { useMemo, useState, useEffect } from 'react';
 import { WeatherContext } from './WeatherContext.jsx';
+/**
+ * A context provider component that provides state and functions for managing weather location, unit, and time.
+ *
+ * @param {Object} props - The component props.
+ * @param {ReactNode} props.children - The child components.
+ * @return {ReactElement} The rendered context provider component.
+ */
 export default function WeatherProvider({ children }) {
 	const [lat, setLat] = useState(null);
 	const [lon, setLon] = useState(null);
@@ -14,6 +21,12 @@ export default function WeatherProvider({ children }) {
 	}, [unit]);
 
 	useEffect(() => {
+		/**
+		 * Handles the storage event when the unit key is updated in localStorage.
+		 *
+		 * @param {StorageEvent} e - The storage event object.
+		 * @return {void}
+		 */
 		const onStorage = (e) => {
 			if (e.key === 'unit' && e.newValue) setUnit(e.newValue);
 		};
