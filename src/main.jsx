0bff0acce5bae20de27a9e4cb8c7daf691a05c98
nieckita <niecki.tehrany.bildung@gmail.com>
2025-08-15 10:05:35 +0200

    add weather provider hier and löchen von app

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import BackgroundLive from './components/BackgroundLive.jsx';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import WeatherProvider from './context/WeatherProvider.jsx';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -17,9 +18,11 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<BackgroundLive />
-			<App />
-			<ReactQueryDevtools initialIsOpen={false} />
+			<WeatherProvider>
+				<BackgroundLive />
+				<App />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</WeatherProvider>
 		</QueryClientProvider>
 	</React.StrictMode>
 );
