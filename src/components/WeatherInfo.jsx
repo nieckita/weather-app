ac10e1f56ab14d227a566f300ab8929623e1ba34
nieckita <niecki.tehrany.bildung@gmail.com>
2025-08-15 10:01:08 +0200

    timeCity added and comntiren

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -2,10 +2,29 @@ import WeatherIcon from './WeatherIcon.jsx';
 
 import WeatherAlert from './WeatherAlert.jsx';
 import { useWeather } from '../context/WeatherContext.jsx';
-import { useEffect } from 'react';
 
+import { useEffect } from 'react';
+import { getCityTime } from '../helpers.js';
+
+/**
+ * Renders the weather information component.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.id - The ID of the weather location.
+ * @param {string} props.name - The name of the weather location.
+ * @param {number} props.temp_max - The maximum temperature in Celsius.
+ * @param {number} props.temp_min - The minimum temperature in Celsius.
+ * @param {number} props.wind - The wind speed in kilometers per hour.
+ * @param {number} props.humidity - The humidity percentage.
+ * @param {number} props.lon - The longitude of the weather location.
+ * @param {number} props.lat - The latitude of the weather location.
+ * @param {string} props.desc - The weather description.
+ * @param {number} props.timezone - The timezone offset in seconds.
+ * @param {number} props.feels_like - The "feels like" temperature in Celsius.
+ * @param {number} props.iconId - The ID of the weather icon.
+ * @return {JSX.Element} The rendered weather information component.
+ */
 export default function WeatherInfo({
-	id,
 	name,
 	temp_max,
 	temp_min,
@@ -18,17 +37,9 @@ export default function WeatherInfo({
 	feels_like,
 	iconId,
 }) {
-	const { setLat, setLon, setTimeZone, setCity } = useWeather();
-	const ctx = useWeather();
+	const { setLat, setLon, setTimeZone, setCity, setcityTime } = useWeather();
 
-	console.log(setLat, setLon, setTimeZone, setCity);
-
-	console.log('types:', {
-		setLat: typeof ctx.setLat,
-		setLon: typeof ctx.setLon,
-		setTimeZone: typeof ctx.setTimeZone,
-		setCity: typeof ctx.setCity,
-	});
+	const cityTime = getCityTime(timezone);
 
 	const unit = localStorage.getItem('unit');
 	if (unit !== 'C°') {
@@ -41,8 +52,22 @@ export default function WeatherInfo({
 		if (lat != null) setLat(lat);
 		if (lon != null) setLon(Number(lon));
 		if (name) setCity(name);
-		if (timezone) setTimeZone(timezone);
-	}, [lat, lon, name, timezone, setLat, setLon, setCity, setTimeZone]);
+		if (timezone !== undefined && timezone !== null) {
+			setTimeZone(timezone);
+			setcityTime(getCityTime(timezone));
+		}
+	}, [
+		lat,
+		lon,
+		name,
+		timezone,
+		cityTime,
+		setLat,
+		setLon,
+		setCity,
+		setTimeZone,
+		setcityTime,
+	]);
 
 	return (
 		<div className="weather-info ">
@@ -75,6 +100,10 @@ export default function WeatherInfo({
 	);
 }
 
+/**
+ * Converts Celsius to Fahrenheit.
+
+ */
 function cToF(celsius) {
 	const f = (Number(celsius) * 9) / 5 + 32;
 	localStorage.setItem('unit', 'F°');
