0c70eda38eb5a125044127439bd16628ee2432e3
nieckita <niecki.tehrany.bildung@gmail.com>
2025-08-15 10:03:47 +0200

    added weather provider für context zu allen componenten

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Layout from './components/Layout.jsx';
 import MainContent from './components/MainContent.jsx';
-import { WeatherProvider } from './context/WeatherContext.jsx';
+
 export default function App() {
 	return (
 		<>
-			<WeatherProvider>
-				<Layout>
-					<MainContent />
-				</Layout>
-			</WeatherProvider>
+			<Layout>
+				<MainContent />
+			</Layout>
 		</>
 	);
 }
