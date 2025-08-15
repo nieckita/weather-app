f9872f55805c630ec1ec37f7798334e6be58a02e
nieckita <niecki.tehrany.bildung@gmail.com>
2025-08-15 10:38:41 +0200

    edit and sauber code

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -30,18 +30,3 @@ export const fetchweatherUrlForcast = up(fetch, () => ({
 	},
 	headers: {},
 }));
-
-// export async function fetchCityApiGeo(lat, lon) {
-// 	try {
-// 		const urlLocaleApi = await fetch(
-// 			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${VITE_openweathermap_API_KEY_V3}&units=metric&lang=de`
-// 		).then((urlLocaleApi) => urlLocaleApi.json());
-// 		// if (!urlLocaleApi.ok) {
-// 		// 	throw new Error('Fehler beim Laden der Daten!');
-// 		// }
-// 		console.log(urlLocaleApi);
-// 		return urlLocaleApi;
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// }
